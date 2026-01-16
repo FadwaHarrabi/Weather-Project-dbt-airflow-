262dff5066df328c382457537394fd1db97fced1
Harrabi Fadwa <142733588+FadwaHarrabi@users.noreply.github.com>
2026-01-16 19:01:25 +0100

    creation of postgres db and table and add functions to insert data into it

--- api_request/api_request.py
+++ api_request/api_request.py
@@ -16,6 +16,5 @@ api_url=f"https://api.weatherstack.com/current?access_key={api_key}&query=Sfax"
 
 
 def moch_fetch_data():
-    with open("api_request/mock_weather_data.json", "r") as file:
-        data = file.read()
+    
     return {'request': {'type': 'City', 'query': 'Sfax, Tunisia', 'language': 'en', 'unit': 'm'}, 'location': {'name': 'Sfax', 'country': 'Tunisia', 'region': 'Sfax', 'lat': '34.741', 'lon': '10.760', 'timezone_id': 'Africa/Tunis', 'localtime': '2026-01-08 20:22', 'localtime_epoch': 1767903720, 'utc_offset': '1.0'}, 'current': {'observation_time': '07:22 PM', 'temperature': 11, 'weather_code': 113, 'weather_icons': ['https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0008_clear_sky_night.png'], 'weather_descriptions': ['Clear '], 'astro': {'sunrise': '07:25 AM', 'sunset': '05:23 PM', 'moonrise': '11:00 PM', 'moonset': '10:40 AM', 'moon_phase': 'Waning Gibbous', 'moon_illumination': 75}, 'air_quality': {'co': '212.85', 'no2': '9.25', 'o3': '54', 'so2': '7.25', 'pm2_5': '11.25', 'pm10': '19.15', 'us-epa-index': '1', 'gb-defra-index': '1'}, 'wind_speed': 23, 'wind_degree': 256, 'wind_dir': 'WSW', 'pressure': 1028, 'precip': 0, 'humidity': 62, 'cloudcover': 25, 'feelslike': 9, 'uv_index': 0, 'visibility': 10, 'is_day': 'no'}}
--- api_request/insert_records.py
+++ api_request/insert_records.py
@@ -4,10 +4,69 @@ import psycopg2
 def connect_to_db():
     print("connecting to postressql database ...")
     try:
-        conn=psycopg2.connect(host="localhost", port="5000", database="weather_data", user="postgres", password="system")
-        print(conn)
+        conn=psycopg2.connect(
+            host="localhost",
+            port="5000",
+            database="weather_data", 
+            user="postgres", 
+            password="system")
+        return conn
     except psycopg2.Error as e:
         print(f"Error connecting to database: {e}")
         raise
+def create_table(conn):
+    print("Creating table if not exists ...")
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE SCHEMA IF NOT EXISTS dev;
+            CREATE TABLE IF NOT EXISTS dev.raw_weather_data (
+                id SERIAL PRIMARY KEY,
+                city VARCHAR(50),
+                temperature FLOAT,
+                weather_description VARCHAR(100),
+                wind_speed FLOAT,
+                time TIMESTAMP,
+                inserted_at TIMESTAMP default NOW(),
+                utc_offset TEXT);
+            """)
+        conn.commit()
+        print("table was created successfully ...")
+    except psycopg2.Error as e:
+        print(f"Error creating table: {e}")
+        raise
 
-connect_to_db()
+
+
+def insert_data(conn,data):
+    print("Inserting data into the table ...")
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO dev.raw_weather_data (city, temperature, weather_description, wind_speed, time, utc_offset)
+            VALUES (%s, %s, %s, %s, %s, %s);
+            """, (
+                data['location']['name'],
+                data['current']['temperature'],
+                data['current']['weather_descriptions'][0],
+                data['current']['wind_speed'],
+                data['location']['localtime'],
+                data['location']['utc_offset']
+            ))
+        conn.commit()
+        print("Data inserted successfully ...")
+    except psycopg2.Error as e:
+        print(f"Error inserting data: {e}")
+        raise
+def main():
+    try:
+        data=moch_fetch_data()
+        conn=connect_to_db()
+        create_table(conn)
+        insert_data(conn,data)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        if 'conn' in locals() :
+            conn.close()
+            print("Database connection closed.")
